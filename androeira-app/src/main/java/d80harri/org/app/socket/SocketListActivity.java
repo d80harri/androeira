9d5e3a3c097ec9fff66612556b8fefb25cbacc5a
Harald Bittermann <harald.bittermann@gmail.com>
2016-06-18 20:13:20 +0200

    Lists available services

--- androeira-app/src/main/java/d80harri/org/app/socket/SocketListActivity.java
+++ androeira-app/src/main/java/d80harri/org/app/socket/SocketListActivity.java
@@ -1,16 +1,20 @@
 package d80harri.org.app.socket;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import d80harri.org.app.R;
 import org.d80harri.androeira.socket.client.ServiceLocator;
 import org.d80harri.androeira.socket.intf.ServiceLocation;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -51,8 +55,7 @@ public class SocketListActivity extends ListActivity {
         text = (TextView) findViewById(R.id.mainText);
 
         // initiate the listadapter
-        adapter = new ArrayAdapter<ServiceLocation>(this,
-                R.layout.socket_list_row, R.id.listText, locations);
+        adapter = new SocketListAdapter(this, locations);
 
 
         // assign the list adapter
@@ -82,5 +85,27 @@ public class SocketListActivity extends ListActivity {
         text.setText("You clicked " + selectedItem + " at position " + position);
     }
 
+    public static class SocketListAdapter extends ArrayAdapter<ServiceLocation> {
+
+        private final LayoutInflater inflater;
+
+        public SocketListAdapter(Context context, List<ServiceLocation> locations) {
+            super(context, R.layout.socket_list_row, locations);
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ServiceLocation item = this.getItem(position);
+            View view = convertView;
+            if (view == null)
+                view = inflater.inflate(R.layout.socket_list_row, null);
+
+            TextView listText = (TextView) view.findViewById(R.id.listText);
+            listText.setText(item.getName());
+
+            return view;
+        }
+    }
 
 }
